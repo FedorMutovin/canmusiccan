ae2d826a1e1300ab9e6ff3d8c8a34abbda79504b
Anakin Skywalker <mutovin9@mail.ru>
2021-01-03 09:57:25 -0500

    "add bootstrap"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../stylesheets/application'
+import './bootstrap_custom.js'
 
 Rails.start()
 Turbolinks.start()
